5674abf9c77190d37ec47dc149bcba8bcd1cbc2a
LeeGMin <yumi3545@naver.com>
2026-05-08 04:45:44 +0000

    fix: 자동으로 생성되어야 할 boundary값이 하드 코딩 되어있어 헤더에 제대로 된 정보를 전달하지 못하는 오류 수정

--- src/entities/profile/api/profile.api.ts
+++ src/entities/profile/api/profile.api.ts
@@ -21,9 +21,5 @@ export const patchChangeProfile = async (data: ChangeProfileRequest) => {
     formData.append('file', data.file);
   }
 
-  return axios.patch('/users/me', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  return axios.patch('/users/me', formData);
 };
